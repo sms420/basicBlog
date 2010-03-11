2703bb76e5cce9a9288a9fce467fff5d00dcefce
Sean <sean.txt@gmail.com>
2010-03-11 00:08:31 -0800

    g: 'recent posts function'

--- db/schema.rb
+++ db/schema.rb
@@ -6,7 +6,7 @@
 # to create the application database on another system, you should be using db:schema:load, not running
 # all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
-#
+# DUDE
 # It's strongly recommended to check this file into your version control system.
 
 ActiveRecord::Schema.define(:version => 20100309042554) do
